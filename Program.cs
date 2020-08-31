60d00847f1280892f59a4a74bf6264379999db39
BillyTheCoder <billydramis@yahoo.com>
2020-08-31 11:13:42 +0100

    test project

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class parent
+    {
+        public virtual void method()
+        {
+            Console.WriteLine("This is parent method");
+        }
+    }
+    class child : parent
+    {
+        public override void method()
+        {
+            Console.WriteLine("This child method");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            parent[] parentClasses = new parent[2];
+
+            parentClasses[0] = new parent();
+            parentClasses[1] = new child();
+
+            foreach(parent on in parentClasses)
+            {
+                on.method();
+            }
+
+
+
+
+
+
+        }
+    }
+}
